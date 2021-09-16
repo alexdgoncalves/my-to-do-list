1f370b2ec2d5aca83758337532219ae040f24111
alexdgoncalves <alex.d.goncalves21@gmail.com>
2021-09-15 21:24:01 -0300

    create finish card

--- src/js/index.js
+++ src/js/index.js
@@ -4,20 +4,16 @@ const bord = document.querySelector("#bord");
 const tarefas = document.querySelector("#tarefas");
 
 adcTarefas.addEventListener("click", () => {
-  const id = Math.random().toString(36).substr(2);
+  const idDelete = Math.random().toString(36).substr(2);
+  const idCheck = Math.random().toString(36).substr(2);
+
   const novaTarefa = inputTarefas.value;
   let card = `
   <li class="li-no">
-    <div class="box-img">
-      <img
-      id="checkbox-${novaTarefa.length}"
-      class="no-checkbox"
-      src="./img/empty-checkbox.png"
-      alt="empty-checkbox"
-      />
+    <div class="box-img" id="${idCheck}">
       <text class="text-no">${novaTarefa}</text>
     </div>
-    <img class="close" id="${id}" src="./img/x.png" />
+    <img class="close" id="${idDelete}" src="./img/x.png" />
   </li>`;
   if (!novaTarefa) {
     alert("campo não preenchido");
@@ -27,21 +23,21 @@ adcTarefas.addEventListener("click", () => {
 
   bord.insertAdjacentHTML("beforeend", card);
 
-  const closeCard = document.getElementById(`${id}`);
-  console.log(id);
-
+  const closeCard = document.getElementById(`${idDelete}`);
   const btnFinalizar = tarefas.lastElementChild.lastChild;
+  const checkbox = document.getElementById(`${idCheck}`);
+
   closeCard.addEventListener("click", () => {
-    if (id === closeCard.id) {
+    if (idDelete === closeCard.id) {
       btnFinalizar.remove();
     }
   });
 
-  const checkbox = document.querySelector(`#checkbox-${novaTarefa.length}`);
-  const textNo = document.querySelector(".li-no");
-
-  checkbox.addEventListener("click", () => {
-    textNo.classList.remove("li-no");
-    textNo.classList.add("li-yes");
+  btnFinalizar.addEventListener("click", () => {
+    if (btnFinalizar.className == "li-no" && checkbox.id == idCheck) {
+      btnFinalizar.className = "li-yes";
+    } else {
+      btnFinalizar.className = "li-no";
+    }
   });
 });
